cd756cd5157675fd0adfe3b62e5e953de64158c4
Joshua Barlow <jalapenojosh@gmail.com>
2021-11-04 19:48:29 -0600

    Piece interface dummy

--- DinoClassLib/Piece.cs
+++ DinoClassLib/Piece.cs
@@ -1,12 +1,21 @@
 using System;
+using Position.cs;
 
 namespace DinoClassLib
 {
     interface Piece
     {
+        private Position();  //protected? or private
+        private int pointVal;
+
+        //constructor?
+
+        public void onFrameUpdate()
+        {
+
+        }
+
 
     }
-    public class piece : Piece
-    {
-    }
+
 }
