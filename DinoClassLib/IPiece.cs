f61943289fe07c8165685ff48cd5fd02059457b1
Stormgate1998 <jalapenojosh@gmail.com>
2021-11-06 19:47:46 -0600

    Small rock preliminary build. I realized that we don't have any way to indicate size, maybe the interface needs to be updated to include a second position object for the top right corner. No tests yet.

--- DinoClassLib/IPiece.cs
+++ DinoClassLib/IPiece.cs
@@ -14,4 +14,30 @@ namespace DinoClassLib
         }
         public void onFrameUpdate();
     }
+
+    public class SmallRock : IPiece { 
+        
+        public Position position
+        {
+            get;
+            set;
+        }
+
+        public int pointVal
+        {
+            get;
+        }
+
+        SmallRock(int x)
+        {
+            position = new Position(x,0);
+            pointVal = 8;
+        }
+    public void onFrameUpdate()
+        {
+            int x = position.getX();//getting the current x value
+            position.setX(x-1);//assuming bottom left is (0,0)
+        }
+    }
+
 }
