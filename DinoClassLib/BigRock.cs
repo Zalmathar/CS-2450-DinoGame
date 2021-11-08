5d3482672fe70d9e7d2ff75f814f7c357789131b
Ty Mortimer <tyorionmortimer@gmail.com>
2021-11-07 19:30:49 -0700

    implemented BigRock class

---
+++ DinoClassLib/BigRock.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DinoClassLib
+{
+    public class BigRock : Ipiece
+    {
+
+        public BigRock(Position po)
+        {
+            position.set(po);
+            pointVal.set(10);
+            xSize.set(1);
+            ySize.set(2);
+        }
+
+        public void onFrameUpdate()
+        {
+            position.setX(position.getX - 1);
+        }
+
+    }
+
+}
--- DinoClassLib/IPiece.cs
+++ DinoClassLib/IPiece.cs
@@ -7,19 +7,23 @@ namespace DinoClassLib
         Position position
         {
             get;
+            set;
         }
         int pointVal
         {
             get;
+            set;
         }
         int xSize
         {
             get;
+            set;
         }
 
         int ySize
         {
             get;
+            set;
         }
         public void onFrameUpdate();
     }
