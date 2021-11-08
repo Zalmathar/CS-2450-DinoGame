856d16a80be19cb4c3f9afc8dde17f830ad83a7e
Ty Mortimer <tyorionmortimer@gmail.com>
2021-11-08 10:29:06 -0700

    big rock

--- DinoClassLib/IPiece.cs
+++ DinoClassLib/IPiece.cs
@@ -25,6 +25,81 @@ namespace DinoClassLib
             get;
             set;
         }
-        public void onFrameUpdate();
+
+        public int pointVal
+        {
+            get;
+        }
+        public int xSize
+        {
+            get;
+        }
+
+        public int ySize
+        {
+            get;
+        }
+
+        public SmallRock(int x, int y)
+        {
+            position = new Position(x,y);
+            xSize = 1;
+            ySize = 1;
+            pointVal = 8;
+            if(position.getY() != 1 || position.getX() > 50 || position.getX() <= 1)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    public void onFrameUpdate()
+        {
+            int x = position.getX();//getting the current x value
+            position.setX(x-1);//assuming bottom left is (0,0)
+
+        }
+    }
+
+
+
+
+    public class Bird : IPiece { 
+        
+        public Position position
+        {
+            get;
+            set;
+        }
+
+        public int pointVal
+        {
+            get;
+        }
+        public int xSize
+        {
+            get;
+        }
+
+        public int ySize
+        {
+            get;
+        }
+
+        public Bird(int x, int y)
+        {
+            position = new Position(x,y);
+            xSize = 1;//unsure about this, clarify later
+            ySize = 1;
+            pointVal = 8;
+            if(position.getY() != 3 || position.getY() != 4 || position.getX() > 50 || position.getX() <= 1)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    public void onFrameUpdate()
+        {
+            int x = position.getX();//getting the current x value
+            position.setX(x-1);//assuming bottom left is (0,0)
+
+        }
     }
 }
