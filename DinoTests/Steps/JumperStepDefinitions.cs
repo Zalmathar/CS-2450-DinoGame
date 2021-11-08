e4f8b0e55d8a7130ce7b9d1a3e3eeb0580ddf5d2
Stormgate1998 <jalapenojosh@gmail.com>
2021-11-08 12:42:15 -0700

    added player object and basic features, no jump.

--- DinoTests/Steps/JumperStepDefinitions.cs
+++ DinoTests/Steps/JumperStepDefinitions.cs
@@ -192,5 +192,63 @@ namespace DinoTests.Steps
             ScenarioContext.Current.Pending();
         }
 
+        //player tests- no jump
+        [Given(@"a player at \((.*), (.*)\)")]
+        public void GivenAPlayerAt(int p0, int p1)
+        {
+            x = p0;
+            y = p1;
+        }
+        [When(@"a player is constructed")]
+        public void WhenAPlayerIsConstructed()
+        {
+            try
+            {
+                sc_.Add("player", new Player(x, y));
+            }
+            catch (Exception e)
+            {
+                sc_.Add("Exception", e);
+            }
+        }
+
+        [Then(@"player Position x is (.*)")]
+        public void ThenPlayerPositionXIs(int p0)
+        {
+            sc_["player"].As<Player>().position.getX().Should().Be(p0);
+        }
+
+        [Then(@"player Position y is (.*)")]
+        public void ThenPlayerPositionYIs(int p0)
+        {
+            sc_["player"].As<Player>().position.getY().Should().Be(p0);
+        }
+
+        [Then(@"player score is set to (.*)")]
+        public void ThenPlayerScoreIsSetTo(int p0)
+        {
+            sc_["player"].As<Player>().pointVal.Should().Be(p0);
+        }
+
+        [When(@"a player frame update happens")]
+        public void WhenAPlayerFrameUpdateHappens()
+        {
+            sc_["player"].As<Player>().onFrameUpdate();
+        }
+
+
+        [Given(@"a player constructed at \((.*), (.*)\)")]
+        public void GivenAPlayerConstructedAt(int p0, int p1)
+        {
+            try
+            {
+                sc_.Add("player", new Player(p0, p1));
+            }
+            catch (Exception e)
+            {
+                sc_.Add("Exception", e);
+            }
+        }
+
     }
 }
