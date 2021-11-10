2403837c132b1c3d83797dd4e79ebb5a2f7572ae
zalmathar <jeditanis@gmail.com>
2021-11-10 11:25:12 -0700

    Implemented DeleteObstacle

--- DinoClassLib/Controller.cs
+++ DinoClassLib/Controller.cs
@@ -42,23 +42,26 @@ namespace DinoClassLib
         // Generates a new game state representing the next frame. 
         public void FrameUpdate()
         {
-            int[] temp = new int[1];//to be replaced once delete obstacle implemented
+            List<int> delList = new List<int>();//to be replaced once delete obstacle implemented
             player.onFrameUpdate(); // Want to update player before the pieces for CheckCollision to work
-            foreach(IPiece piece in Obstacles)
+            for(int i = 0; i < Obstacles.Count; i++)
             {
+                IPiece piece = Obstacles[i];
                 piece.onFrameUpdate();
                 if(CheckCollision(piece) == true)
                 {
                     gameState = status.dead;
                     // Display the end game card
                 }
-
+                if(piece.position.getX() < 1)
+                {
+                    delList.Add(i); // Mark the obstacle to be deleted
+                }
             }
             Playerjump();
            // CheckCollision();
-            //DeleteObstacle(temp);
-
-            //MakeObstacle();
+            DeleteObstacle(delList);
+            // MakeObstacle();
             //Unsure if this next part is my job
             // TODO: Display the game state to the user.
             
@@ -108,14 +111,17 @@ namespace DinoClassLib
             }   
         }
         // When an obsticale has reached the minimum it can be (Far left of the view). Remove each obsticale that has reached the end of the screen
-        private void DeleteObstacle(int[] delThese)
+        private void DeleteObstacle(List<int> delThese)
         {
             // TODO: Delete each obstacle that is specified by delThese
             //To do this im thinking of in this iteration loop, store the index of each item that needs to be deleted. 
                  // Than deleting them from the largest index to the smallest. IE.If the obstacles stored at index 1, 3, and 10 need to be deleted. Delete them in order 10, 3, then 1.
                  // Deleteing them in reverse would preserve the integrity of the list as we delete multiple objects from it.Minimizing the amount of times we have to iterate through our obstacle list.
                 //  We would then pass this array of indexes to be deleted to our delete method that would go in and delete them. -- Tanis Olesen
-            throw new NotImplementedException();
+            for(int i = delThese.Count; i > 0; i--)
+            {
+                Obstacles.RemoveAt(delThese[i]);
+            }
         }
 
         private void Playerjump()
