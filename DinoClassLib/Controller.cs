b609aa78109fb23f51f545ea8cbe46cf75c4778b
Joshua Barlow <jalapenojosh@gmail.com>
2021-11-09 20:59:24 -0700

    Most of frame update, not currently working

--- DinoClassLib/Controller.cs
+++ DinoClassLib/Controller.cs
@@ -10,9 +10,11 @@ namespace DinoClassLib
     {
 
         // Represents the player
-        private Player player;
+        public Player player = new Player(3, 1);
+
+        public bool Jumpinfo = false;// the IO sets it to true if a jump signal is detected
         // Holds a List of all of the obstacles that are on screen
-        public List<IPiece> Obstacles { get; private set;}
+        public List<IPiece> Obstacles { get; set;}
         
 
         // Represents the current score the player of the game has achieved. 
@@ -39,18 +41,19 @@ namespace DinoClassLib
         // Generates a new game state representing the next frame. 
         public void FrameUpdate()
         {
-            // TODO: Go through each obstical and update is position.
-            // TODO: Check to see if the player is jumping, if it is update the players state.
-            // TODO: Check for collisions, If collisions are detected end the game, and display the end game card. If not continue the game. 
-            // TODO: Add any score for each obstacle that has been cleared by the player. or just one point for an empty space
-            /* TODO: Any Obstacle that has reached the far left of the view must get deleted.
-                  To do this im thinking of in this iteration loop, store the index of each item that needs to be deleted. 
-                  Than deleting them from the largest index to the smallest. IE. If the obstacles stored at index 1, 3, and 10 need to be deleted. Delete them in order 10, 3, then 1. 
-                  Deleteing them in reverse would preserve the integrity of the list as we delete multiple objects from it. Minimizing the amount of times we have to iterate through our obstacle list. 
-                  We would then pass this array of indexes to be deleted to our delete method that would go in and delete them. -- Tanis Olesen
-            */
-            // TODO: Randomly generate a new obsticales
+            int[] temp = new int[1];//to be replaced once delete obstacle implemented
+            foreach(IPiece piece in Obstacles)
+            {
+                piece.onFrameUpdate();
+
+            }
+            Playerjump();
+            player.onFrameUpdate();
+            CheckCollision();
+            DeleteObstacle(temp);
+
             MakeObstacle();
+            //Unsure if this next part is my job
             // TODO: Display the game state to the user.
             
         }
@@ -89,7 +92,19 @@ namespace DinoClassLib
         private void DeleteObstacle(int[] delThese)
         {
             // TODO: Delete each obstacle that is specified by delThese
+            //To do this im thinking of in this iteration loop, store the index of each item that needs to be deleted. 
+                 // Than deleting them from the largest index to the smallest. IE.If the obstacles stored at index 1, 3, and 10 need to be deleted. Delete them in order 10, 3, then 1.
+                 // Deleteing them in reverse would preserve the integrity of the list as we delete multiple objects from it.Minimizing the amount of times we have to iterate through our obstacle list.
+                //  We would then pass this array of indexes to be deleted to our delete method that would go in and delete them. -- Tanis Olesen
             throw new NotImplementedException();
         }
+
+        private void Playerjump()
+        {
+            player.IsJumping = Jumpinfo;
+            Jumpinfo = false;
+        }
+
+        
     }
 }
