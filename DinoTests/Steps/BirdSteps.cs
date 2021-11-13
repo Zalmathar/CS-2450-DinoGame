c33e720428d735359a5f6ae65893ca902cc37079
zalmathar <jeditanis@gmail.com>
2021-11-12 18:13:53 -0700

    Migrated bird tests from JumperStepDefinitions to BirdSteps.cs

--- DinoTests/Steps/BirdSteps.cs
+++ DinoTests/Steps/BirdSteps.cs
@@ -14,5 +14,63 @@ namespace DinoTests.Steps
         {
             sc = scenarioContext;
         }
+
+        [Given(@"a bird at \((.*), (.*)\)")]
+        public void GivenABirdAt(int p0, int p1)
+        {
+            x = p0;
+            y = p1;
+        }
+
+        [Given(@"a bird constructed at \((.*), (.*)\)")]
+        public void GivenABirdConstructedAt(int p0, int p1)
+        {
+            try
+            {
+                sc.Add("bird", new Bird(p0, p1));
+            }
+            catch (Exception e)
+            {
+                sc.Add("Exception", e);
+            }
+        }
+
+        [When(@"a bird is constructed")]
+        public void WhenABirdIsConstructed()
+        {
+            try
+            {
+                sc.Add("bird", new Bird(x, y));
+            }
+            catch (Exception e)
+            {
+                sc.Add("Exception", e);
+            }
+        }
+
+        [When(@"a bird frame update happens")]
+        public void WhenABirdFrameUpdateHappens()
+        {
+            sc["bird"].As<Bird>().onFrameUpdate();
+        }
+
+
+        [Then(@"bird Position x is (.*)")]
+        public void ThenBirdPositionXIs(int p0)
+        {
+            sc["bird"].As<Bird>().position.getX().Should().Be(p0);
+        }
+
+        [Then(@"bird Position y is (.*)")]
+        public void ThenBirdPositionYIs(int p0)
+        {
+            sc["bird"].As<Bird>().position.getY().Should().Be(p0);
+        }
+
+        [Then(@"bird score is set to (.*)")]
+        public void ThenBirdScoreIsSetTo(int p0)
+        {
+            sc["bird"].As<Bird>().pointVal.Should().Be(p0);
+        }
     }
 }
--- DinoTests/Steps/JumperStepDefinitions.cs
+++ DinoTests/Steps/JumperStepDefinitions.cs
@@ -53,63 +53,12 @@ namespace DinoTests.Steps
 
         //bird tests start
 
-        [Given(@"a bird at \((.*), (.*)\)")]
-        public void GivenABirdAt(int p0, int p1)
-        {
-            x = p0;
-            y = p1;
-
-        }
-        [When(@"a bird is constructed")]
-        public void WhenABirdIsConstructed()
-        {
-            try
-            {
-                sc.Add("bird", new Bird(x, y));
-            }
-            catch (Exception e)
-            {
-                sc.Add("Exception", e);
-            }
-        }
 
 
-        [Then(@"bird Position x is (.*)")]
-        public void ThenBirdPositionXIs(int p0)
-        {
-            sc["bird"].As<Bird>().position.getX().Should().Be(p0);
-        }
-
-        [Then(@"bird Position y is (.*)")]
-        public void ThenBirdPositionYIs(int p0)
-        {
-            sc["bird"].As<Bird>().position.getY().Should().Be(p0);
-        }
-
-        [Then(@"bird score is set to (.*)")]
-        public void ThenBirdScoreIsSetTo(int p0)
-        {
-            sc["bird"].As<Bird>().pointVal.Should().Be(p0);
-        }
-        [When(@"a bird frame update happens")]
-        public void WhenABirdFrameUpdateHappens()
-        {
-            sc["bird"].As<Bird>().onFrameUpdate();
-        }
 
 
-        [Given(@"a bird constructed at \((.*), (.*)\)")]
-        public void GivenABirdConstructedAt(int p0, int p1)
-        {
-            try
-            {
-                sc.Add("bird", new Bird(p0, p1));
-            }
-            catch (Exception e)
-            {
-                sc.Add("Exception", e);
-            }
-        }
+
+
 
         //player tests
         [Given(@"a player at \((.*), (.*)\)")]
